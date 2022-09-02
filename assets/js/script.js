bc58b6411687478eb9cb0aa1a8c8525548526bef
Ari-chan <arielle.elak@gmail.com>
2022-09-02 16:36:55 -0600

    Establishing variables for grabbing data from local storage and posting to page, reorganizing logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,63 @@
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  */
 
-// !TODO: On page load - display current day at the top of the calendar
+
+var eventsObj = {
+    "9am": "",
+    "10am": "",
+    "11am": "",
+    "12pm": "",
+    "1pm": "",
+    "2pm": "",
+    "3pm": "",
+    "4pm": "",
+    "5pm": "",
+};
+
+/**
+ * When the page is fully ready, grab the latest info from local storage
+ * If nothing exists, create an empty array for eventsArr
+ */
+$(function () {
+    var eventsArr = JSON.parse(localStorage.getItem("eventsObj")) || [];
+    console.log("Retreived eventsObj from localStorage" + eventsArr);
+});
+
+// !TODO: Generate rows for each hour
+for (const key in eventsObj) {
+    if (eventsObj.hasOwnProperty(key)) {
+        console.log(`${key}: ${eventsObj.getItem(key)}`);
+    }
+};
+
+
+
+// Pull current list of events from local storage object
+ var retrievedEvents = localStorage.getItem('testObject');
+
+// Write the current contents of eventsObj to local storage
+localStorage.setItem('eventsObj', JSON.stringify(eventsObj));
+
+// Empty events object is sent to local storage if no data exists
+var eventsObj = {
+    "9am": "",
+    "10am": "",
+    "11am": "",
+    "12pm": "",
+    "1pm": "",
+    "2pm": "",
+    "3pm": "",
+    "4pm": "",
+    "5pm": "",
+};
+
+
+// On page load - display current day at the top of the calendar
 var currentDay = moment().format("dddd, MMMM Do, YYYY");
 $("#currentDay").text(currentDay);
 
-
-
-
-
 // !TODO: Display timeblocks for standard business hours 9am - 5pm by the hour
+
 
 
 /**
@@ -22,6 +70,41 @@ $("#currentDay").text(currentDay);
  * - Present (= Today) orange
  * - Future (> Today) green
  */
+
+
+
+// Set up variables for each event section
+function showScheduleRow(hour, description, save) {
+    var eventSection = '';
+    var rowEl = '';
+    var hourEl = '';
+    var descriptionEl = '';
+    var buttonEl = '';
+};
+
+
+
+
+/*
+<section class="time-block">
+        <div class="row" id="9am">
+          <div class="hour col-2 col-md-2 col-lg-2">
+            9AM
+          </div>
+          <div class="description col-8 col-md-9 col-lg-9 past">
+            Morning Meeting
+          </div>
+          <button class="saveBtn col-2 col-md-1 col-lg-1">
+            <i class="fa fa-save"></i>
+          </button>
+          </div>
+        </div>
+      </section>
+*/
+
+ // !TODO: Generate text based upon the current eventsObj contents
+
+
 
 // !TODO: On click event for each timeblock - space converts to text input
 
@@ -63,20 +146,7 @@ $("#currentDay").text(currentDay);
  *
  * Mock time block structure:
  *
- * <section class="time-block">
-        <div class="row" id="9am">
-          <div class="hour col-2 col-md-2 col-lg-2">
-            9AM
-          </div>
-          <div class="description col-8 col-md-9 col-lg-9 past">
-            Morning Meeting
-          </div>
-          <button class="saveBtn col-2 col-md-1 col-lg-1">
-            <i class="fa fa-save"></i>
-          </button>
-          </div>
-        </div>
-      </section>
+ *
  *
  */
 
