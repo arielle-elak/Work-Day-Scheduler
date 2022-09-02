69397e932d32ddc9b96e809aae3b973cb64852b4
Ari-chan <arielle.elak@gmail.com>
2022-09-02 14:18:56 -0600

    JS TODOs entered

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+/**
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Work Day Scheduler JS
+ * Last Edited by Arielle Sept 2 2022
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+// !TODO: On page load - display current day at the top of the calendar
+
+// !TODO: Display timeblocks for standard business hours 9am - 5pm by the hour
+
+
+/**
+ * !TODO: Timeblock timeframes color coding
+ * - Past (< Today) grey
+ * - Present (= Today) orange
+ * - Future (> Today) green
+ */
+
+// !TODO: On click event for each timeblock - space converts to text input
+
+/**
+ *!TODO: On click event for save button - the text input is saved to local storage
+ * Empty local storage input:
+ *
+ * eventsObj {};
+ *
+ * Empty array"
+ *
+ * eventsArr [];
+ *
+ * When each event is saved, for the respective timeblock, append to the events object
+ *
+ * eventsObj {
+ *  "9am": "",
+ *  "10am": "",
+ *  "11am": "",
+ *  "12pm": "",
+ *  "1pm": "",
+ *  "2pm": "",
+ *  "3pm": "",
+ *  "4pm": "",
+ *  "5pm": "",
+ * }
+ *
+ * eventsArr [
+ *  {"9am": ""},
+ *  {"10am": ""},
+ *  {"11am": ""},
+ *  {"12pm": ""},
+ *  {"1pm": ""},
+ *  {"2pm": ""},
+ *  {"3pm": ""},
+ *  {"4pm": ""},
+ *  {"5pm": ""},
+ * ]
+ */
